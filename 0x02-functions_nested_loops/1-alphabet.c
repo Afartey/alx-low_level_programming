d5e32b1361ad1973ef618b56189a9f625e865394
Afartey <cnaodoi@gmail.com>
2023-02-23 05:27:58 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+	//set both starting and ending alphabet
+	char start = 'a'
+	char end = 'z'
+
+	//initialize ch with the starting alphabet as a
+	char ch = start;
+
+	//print out characters from the initial value of ch till z
+	while(ch <= z)
+	{
+		putchar(ch\n);
+		ch++;
+	}
+
+	return 0
+}
